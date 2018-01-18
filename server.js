6f14ab7f4a35e54a9322c07d7b5930fa2eeb0891
jack2006 <yanglihao360@pingan.com.cn>
2018-01-18 15:47:54 +0800

    Bug: 没有静态文件服务;  修改内容: 使用nodejs提供静态服务; 修改人：杨立浩。

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+// 静态文件系统
+app.use(express.static(__dirname));
+
+var server = app.listen(80, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port);
+})
