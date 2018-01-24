0aaaf080cdbe49a504113d3b7cb2d31391ae7885
jack2006 <yanglihao360@pingan.com.cn>
2018-01-24 16:25:45 +0800

    修改端口为80

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 // 静态文件系统
 app.use(express.static(__dirname));
 
-var server = app.listen(8082, function () {
+var server = app.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log("应用实例，访问地址为 http://%s:%s", host, port);
